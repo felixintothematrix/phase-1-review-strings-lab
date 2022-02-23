9d4ef02e3fb554bffc58b9b40f4e54b7ea41617f
Felix Garcia <felixgarciarattler@gmail.com>
2022-02-23 10:17:58 -0600

    commit tree 1e5125359ba91923cdd05a7eb4f7e841a5f311a1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+console.log(currentUser)
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+console.log(welcomeMessage)
